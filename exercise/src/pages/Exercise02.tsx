603d1dca1f8775d1ed4b3d04da0ad86080f2cf8d
dmhashanwork <120355703+dmhashanwork@users.noreply.github.com>
2023-07-08 10:14:29 +0530

    update exercise code

--- exercise/src/pages/Exercise02.tsx
+++ exercise/src/pages/Exercise02.tsx
@@ -1,8 +1,61 @@
+import { useState } from "react";
+
 const Exercise02 = () => {
+    const [todos, setTodos] = useState<Array<string>>([]);
+    const [inputValue, setInputValue] = useState<string>('');
+    const [editIndex, setEditIndex] = useState<null | number>(null);
+
+    const handleInputChange = (e: any) => {
+        setInputValue(e.target.value);
+    };
+
+    const addTodo = () => {
+        if (inputValue.trim() !== '') {
+            if (editIndex !== null) {
+                const updatedTodos = [...todos];
+                updatedTodos[editIndex] = inputValue;
+                setTodos(updatedTodos);
+                setEditIndex(null);
+            } else {
+                setTodos([...todos, inputValue]);
+            }
+            setInputValue('');
+        }
+    };
+
+    const deleteTodo = (index: number) => {
+        const updatedTodos = todos.filter((_, i) => i !== index);
+        setTodos(updatedTodos);
+    };
+
+    const editTodo = (index: number) => {
+        const todoToEdit = todos[index];
+        setInputValue(todoToEdit);
+        setEditIndex(index);
+    };
+
     return (
         <div className="component-body">
-            <h2>Exercise 02</h2>
-            <p>Implement a Todo App using a state array.</p>
+            <h1>Todo App using State Array</h1>
+            <div className="todo-text-input-div">
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="Add a todo..."
+                    className="todo-text-input"
+                />
+                <button className="todo-text-input-button" onClick={addTodo}>{editIndex !== null ? 'Update' : 'Add'}</button>
+            </div>
+            <ul className="todo-list">
+                {todos.map((todo, index) => (
+                    <li className="todo-list-item" key={index}>
+                        {todo}
+                        <button className="todo-list-item-button" onClick={() => editTodo(index)}>Edit</button>
+                        <button className="todo-list-item-button" onClick={() => deleteTodo(index)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
--- exercise/src/pages/Exercise03.tsx
+++ exercise/src/pages/Exercise03.tsx
@@ -1,38 +1,102 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Exercise03 = () => {
+    const [todos, setTodos] = useState<Array<any>>([]);
+    const [inputValue, setInputValue] = useState<string>('');
+    const [editIndex, setEditIndex] = useState<null | number>(null);
+
+    const client = axios.create({
+        baseURL: "https://jsonplaceholder.typicode.com" 
+     });
+
+     useEffect(() => {
+        fetchTodos();
+    }, []);
+
+    const fetchTodos = async () => {
+        try {
+            const response = await client.get("/todos");
+            setTodos(response.data?.slice(0, 5));
+        } catch (error) {
+            console.error("Error fetching todos:", error);
+        }
+    };
+
+    const handleInputChange = (e: any) => {
+        setInputValue(e.target.value);
+    };
+
+    const addTodo = async () => {
+        if (inputValue.trim() !== "") {
+            try {
+                if (editIndex !== null) {
+                    const updatedTodos = [...todos];
+                    const updatedTodo = { ...updatedTodos[editIndex], title: inputValue };
+                    await client.put(`/todos/${updatedTodo.id}`, updatedTodo);
+                    updatedTodos[editIndex] = updatedTodo;
+                    setTodos(updatedTodos);
+                    setEditIndex(null);
+                } else {
+                    const response = await client.post("/todos", {
+                        title: inputValue,
+                        completed: false,
+                    });
+                    setTodos([...todos, response.data]);
+                }
+                setInputValue("");
+            } catch (error) {
+                console.error("Error adding/editing todo:", error);
+            }
+        }
+    };
+
+    const deleteTodo = async (id: number) => {
+        try {
+            await client.delete(`/todos/${id}`);
+            const updatedTodos = todos.filter((todo) => todo.id !== id);
+            setTodos(updatedTodos);
+        } catch (error) {
+            console.error("Error deleting todo:", error);
+        }
+    };
+
+    const editTodo = (index: number) => {
+        const todoToEdit = todos[index];
+        setInputValue(todoToEdit.title);
+        setEditIndex(index);
+    };
+
     return (
         <div className="component-body">
-            <h2>Exercise 03</h2>
-            <p>Modify the previously developed Todo application using this REST api.</p>
-            <a href="https://jsonplaceholder.typicode.com/todos" target="_blank">https://jsonplaceholder.typicode.com/todos</a>
-
-            <table>
-                <tr>
-                    <td>Listing all todos</td>
-                    <td>GET</td>
-                    <td rowSpan={2}>https://jsonplaceholder.typicode.com/todos</td>
-                    <td rowSpan={4}>{`{
-                        "userId": 1,
-                        "id": 1,
-                        "title": "todo note",
-                        "completed": false
-                    }`}</td>
-                </tr>
-                <tr>
-                    <td>Creating a todo</td>
-                    <td>POST</td>
-                </tr>
-                <tr>
-                    <td>Updating a todo</td>
-                    <td>PUT</td>
-                    <td rowSpan={2}>https://jsonplaceholder.typicode.com/todos/{`{id}`}</td>
-                </tr>
-                <tr>
-                    <td>Deleting a todo</td>
-                    <td>DELETE</td>
-                </tr>
-            </table>
+            <h1>Todo App</h1>
+            <div className="todo-text-input-div">
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="Add a todo..."
+                    className="todo-text-input"
+                />
+                <button className="todo-text-input-button" onClick={addTodo}>
+                    {editIndex !== null ? "Update" : "Add"}
+                </button>
+            </div>
+            <ul className="todo-list">
+                {todos.map((todo, index) => (
+                    <li className="todo-list-item" key={todo.id}>
+                        {todo.title}
+                        <button className="todo-list-item-button" onClick={() => editTodo(index)}>
+                            Edit
+                        </button>
+                        <button className="todo-list-item-button" onClick={() => deleteTodo(todo.id)}>
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
-}
+};
 
 export default Exercise03;
